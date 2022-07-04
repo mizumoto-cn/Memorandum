c625841ffbaee8c5742ba821c76f0ccda7265b72
XU RUIYUAN <mizumotokunn@gmail.com>
2022-07-04 17:14:56 +0900

    demo_main

---
+++ demo_main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/mizumoto-cn/memorandum"
+	"github.com/mizumoto-cn/memorandum/notify"
+	"github.com/mizumoto-cn/memorandum/schedule"
+)
+
+func main() {
+
+	// Schedule an event
+	format := time.RFC1123Z
+	s, _ := schedule.NewSchedule(format, time.Now().Format(format), "Local")
+
+	// Repeat it every 5 seconds
+	r := schedule.IntervalMultiplexer{
+		Interval:      time.Duration(5) * time.Second,
+		NumberOfTimes: 3,
+	}
+	r.Repeat(s)
+	// Create the reminder
+	rem := memorandum.Reminder{
+		Schedule: s,
+		Notifier: &notify.DesktopNotifier{},
+	}
+
+	err := rem.Remind("Reminder", "I'm a reminder made in Go!")
+	fmt.Println(err)
+}
